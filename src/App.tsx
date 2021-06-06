5015bed72ea5abf63b0f4ad8dec46ecd9a366206
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-06-06 07:22:51 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreatePayment from './CreatePayment';
+
+const App = () => {
+  return (
+    <div>
+      <CreatePayment />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/CreatePayment.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePayment = () => {
+  return (
+    <div>
+      CreatePayment
+    </div>
+  )
+}
+
+export default CreatePayment
